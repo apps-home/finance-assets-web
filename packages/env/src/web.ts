854a31e68395a2b5220254a7eaa0571fdb118d18
Lucasdias067 <lucasdias067@gmail.com>
2026-01-21 17:03:59 -0300

    commit

--- packages/env/src/web.ts
+++ packages/env/src/web.ts
@@ -5,7 +5,8 @@ export const env = createEnv({
 	emptyStringAsUndefined: true,
 	server: {
 		BETTER_AUTH_SECRET: z.string().min(1, 'BETTER_AUTH_SECRET is required'),
-		BETTER_AUTH_URL: z.url()
+		BETTER_AUTH_URL: z.url(),
+		DATABASE_URL: z.url()
 	},
 	client: {
 		NEXT_PUBLIC_SERVER_URL: z.url(),
@@ -17,6 +18,7 @@ export const env = createEnv({
 		NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL ?? '',
 		NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH ?? '',
 		BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET ?? '',
-		BETTER_AUTH_URL: process.env.BETTER_AUTH_URL ?? ''
+		BETTER_AUTH_URL: process.env.BETTER_AUTH_URL ?? '',
+		DATABASE_URL: process.env.DATABASE_URL ?? ''
 	}
 })
